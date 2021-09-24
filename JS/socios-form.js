0692541798bb7ccd5e337f0163acf015c419bbff
Luis Fernando Rondan <81602969+luisrondan2001@users.noreply.github.com>
2021-09-23 21:30:09 -0500

    Update socios-form.js

--- JS/socios-form.js
+++ JS/socios-form.js
@@ -1,11 +1,12 @@
-var tablaGame = localStorage.getItem("tablaGameStorage"); // CREA VARIABLE
-tablaGame = JSON.parse(tablaGame);                        // TRANSFORMA A UNA VARIABLE JAVASCRIPT
-if (tablaGame == null) {
-    tablaGame = [];
+var tablaSocios = localStorage.getItem("tablaSociosStorage"); // CREA VARIABLE
+tablaSocios = JSON.parse(tablaSocios);                        // TRANSFORMA A UNA VARIABLE JAVASCRIPT
+if (tablaSocios == null) {
+    tablaSocios = [];
 }
 
-var idForm = localStorage.getItem("idForm");
-idForm = JSON.parse(idForm);
+var idForm = localStorage.getItem("idForm"); // localStorage almacena la información de forma indefinida
+idForm = JSON.parse(idForm); // JSON para almacenar e intercambiar información de texto
+
 if (idForm == null) {
     idForm = 0;
 }
@@ -13,17 +14,17 @@ if (idForm == null) {
 cargarPagina();
 
 function guardar() {
-    Swal.fire({
+    Swal.fire({   // swal, es para guardar alertas personalesisadas
         title: 'GUARDAR',
         html: ' DESEA GUARDAR CAMBIOS?',
         showDenyButton: true,
         confirmButtonText: 'SI',
         denyButtonText: `NO`,
-    }).then((result) => {
+    }).then((result) => {    /* Read more about isConfirmed, isDenied below */
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-            var objGame = JSON.stringify({
-                idJuego: (idForm > 0) ? idForm : (tablaGame.length + 1),
+            var objGame = JSON.stringify({ //stringify, convierte un valor de Js en una cadena de texto
+                idJuego: (idForm > 0) ? idForm : (tablaSocios.length + 1),  // mayor a cero significa que vamos a editar
                 nombre_juego: document.getElementById("txtnombre").value,
                 nombre_titular: document.getElementById("txtapellido").value,
                 empresa: document.getElementById("txtDNI").value,
@@ -35,17 +36,17 @@ function guardar() {
             //GUARDAMOS EN LOCAL STORAGE
             // AGREGA EL OBJETO A LA VARIABLA TABLAPACIENTE
             if (idForm > 0) {
-                for (const i in tablaGame) {
-                    var varGame = JSON.parse(tablaGame[i]);
+                for (const i in tablaSocios) {
+                    var varGame = JSON.parse(tablaSocios[i]);
                     if (varGame.idJuego == idForm) {
-                        tablaGame[i] = objGame;
+                        tablaSocios[i] = objGame;
                         break;
                     }
                 }
-            } else {
-                tablaGame.push(objGame);
+            } else { //para indicar instrucciones a realizar en caso de no cumplirse la condición
+                tablaSocios.push(objGame);
             }
-            localStorage.setItem("tablaGameStorage", JSON.stringify(tablaGame));
+            localStorage.setItem("tablaSociosStorage", JSON.stringify(tablaSocios)); //setItem, Guardamos la informacion 
 
 
             Swal.fire('Se guardaron los datos !', '', 'success').then((result) => {
@@ -60,17 +61,18 @@ function guardar() {
 
 function cargarPagina() {
     if (idForm > 0) {
-        for (const i in tablaGame) {
-            var varGame = JSON.parse(tablaGame[i]);
+        for (const i in tablaSocios) {
+            var varGame = JSON.parse(tablaSocios[i]);
             if (varGame.idJuego == idForm) {
-                document.getElementById("txtIdsocio").value = varGame.idJuego;
-                document.getElementById("txtnombre").value = varGame.nombre_juego;
-                document.getElementById("txtapellido").value = varGame.nombre_titular;
-                document.getElementById("txtdireccion").value = varGame.empresa;
-                document.getElementById("txtDNI").value = varGame.telefono;
+                document.getElementById("txtIdJuego").value = varGame.idJuego;
+                document.getElementById("txtNombreJ").value = varGame.nombre_juego;
+                document.getElementById("txtNombTi").value = varGame.nombre_titular;
+                document.getElementById("txtEmpresa").value = varGame.empresa;
+                document.getElementById("txtTelefono").value = varGame.telefono;
                 document.getElementById("cboEstado").value = varGame.estado;
-                break;
-            }
+                break; //break,es detener la ejecución de un bucle y salirse de él.
+            
+            } 
         }
     }
 }
